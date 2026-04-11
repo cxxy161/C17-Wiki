a6cda313f90e71d43049157cca00a490a8832198
cxxy168 <cxxy7316@gamil.com>
2026-04-11 15:34:04 +0900

    update: astro.config.mjs,.github/, [15:34:04]

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://cxxy161.github.io',
+  base: '/C17-Wiki',
   integrations: [
     starlight({
       title: 'C17星区 Wiki',
